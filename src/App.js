c08f3e25c30cf5c8d0e0bdae5bf5d65a7601026a
Adebola-Christianah <oguntoyeadebola21@gmail.com>
2024-09-16 18:05:55 +0100

    router changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import necessary components
 
+
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
